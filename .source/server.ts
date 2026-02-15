b7903a62b7f4893c840c235c137588440253fa46
mhrjdv <jadhav.mihir5@gmail.com>
2026-02-15 15:03:31 +0530

    fixed prisma vercel issue

--- .source/server.ts
+++ .source/server.ts
@@ -2,8 +2,8 @@
 import { default as __fd_glob_45 } from "../content/docs/web-app/meta.json?collection=meta"
 import { default as __fd_glob_44 } from "../content/docs/platforms/meta.json?collection=meta"
 import { default as __fd_glob_43 } from "../content/docs/getting-started/meta.json?collection=meta"
-import { default as __fd_glob_42 } from "../content/docs/concepts/meta.json?collection=meta"
-import { default as __fd_glob_41 } from "../content/docs/chrome-extension/meta.json?collection=meta"
+import { default as __fd_glob_42 } from "../content/docs/chrome-extension/meta.json?collection=meta"
+import { default as __fd_glob_41 } from "../content/docs/concepts/meta.json?collection=meta"
 import { default as __fd_glob_40 } from "../content/docs/api-reference/meta.json?collection=meta"
 import { default as __fd_glob_39 } from "../content/docs/meta.json?collection=meta"
 import * as __fd_glob_38 from "../content/docs/web-app/timeline.mdx?collection=docs"
@@ -24,15 +24,15 @@ import * as __fd_glob_24 from "../content/docs/platforms/midjourney.mdx?collecti
 import * as __fd_glob_23 from "../content/docs/platforms/index.mdx?collection=docs"
 import * as __fd_glob_22 from "../content/docs/platforms/freepik.mdx?collection=docs"
 import * as __fd_glob_21 from "../content/docs/platforms/elevenlabs.mdx?collection=docs"
-import * as __fd_glob_20 from "../content/docs/getting-started/installation.mdx?collection=docs"
-import * as __fd_glob_19 from "../content/docs/getting-started/index.mdx?collection=docs"
-import * as __fd_glob_18 from "../content/docs/getting-started/first-project.mdx?collection=docs"
-import * as __fd_glob_17 from "../content/docs/getting-started/extension-setup.mdx?collection=docs"
-import * as __fd_glob_16 from "../content/docs/concepts/rights-provenance.mdx?collection=docs"
-import * as __fd_glob_15 from "../content/docs/concepts/prompt-packages.mdx?collection=docs"
-import * as __fd_glob_14 from "../content/docs/concepts/object-model.mdx?collection=docs"
-import * as __fd_glob_13 from "../content/docs/concepts/index.mdx?collection=docs"
-import * as __fd_glob_12 from "../content/docs/concepts/asset-lifecycle.mdx?collection=docs"
+import * as __fd_glob_20 from "../content/docs/concepts/rights-provenance.mdx?collection=docs"
+import * as __fd_glob_19 from "../content/docs/concepts/prompt-packages.mdx?collection=docs"
+import * as __fd_glob_18 from "../content/docs/concepts/object-model.mdx?collection=docs"
+import * as __fd_glob_17 from "../content/docs/concepts/index.mdx?collection=docs"
+import * as __fd_glob_16 from "../content/docs/concepts/asset-lifecycle.mdx?collection=docs"
+import * as __fd_glob_15 from "../content/docs/getting-started/installation.mdx?collection=docs"
+import * as __fd_glob_14 from "../content/docs/getting-started/index.mdx?collection=docs"
+import * as __fd_glob_13 from "../content/docs/getting-started/first-project.mdx?collection=docs"
+import * as __fd_glob_12 from "../content/docs/getting-started/extension-setup.mdx?collection=docs"
 import * as __fd_glob_11 from "../content/docs/chrome-extension/side-panel.mdx?collection=docs"
 import * as __fd_glob_10 from "../content/docs/chrome-extension/reuse.mdx?collection=docs"
 import * as __fd_glob_9 from "../content/docs/chrome-extension/queue.mdx?collection=docs"
@@ -53,6 +53,6 @@ const create = server<typeof Config, import("fumadocs-mdx/runtime/types").Intern
   }
 }>({"doc":{"passthroughs":["extractedReferences"]}});
 
-export const docs = await create.doc("docs", "content/docs", {"index.mdx": __fd_glob_0, "api-reference/authentication.mdx": __fd_glob_1, "api-reference/extension-api.mdx": __fd_glob_2, "api-reference/index.mdx": __fd_glob_3, "api-reference/webhooks.mdx": __fd_glob_4, "chrome-extension/capture.mdx": __fd_glob_5, "chrome-extension/index.mdx": __fd_glob_6, "chrome-extension/installation.mdx": __fd_glob_7, "chrome-extension/platform-detection.mdx": __fd_glob_8, "chrome-extension/queue.mdx": __fd_glob_9, "chrome-extension/reuse.mdx": __fd_glob_10, "chrome-extension/side-panel.mdx": __fd_glob_11, "concepts/asset-lifecycle.mdx": __fd_glob_12, "concepts/index.mdx": __fd_glob_13, "concepts/object-model.mdx": __fd_glob_14, "concepts/prompt-packages.mdx": __fd_glob_15, "concepts/rights-provenance.mdx": __fd_glob_16, "getting-started/extension-setup.mdx": __fd_glob_17, "getting-started/first-project.mdx": __fd_glob_18, "getting-started/index.mdx": __fd_glob_19, "getting-started/installation.mdx": __fd_glob_20, "platforms/elevenlabs.mdx": __fd_glob_21, "platforms/freepik.mdx": __fd_glob_22, "platforms/index.mdx": __fd_glob_23, "platforms/midjourney.mdx": __fd_glob_24, "platforms/runway.mdx": __fd_glob_25, "platforms/sora.mdx": __fd_glob_26, "platforms/suno.mdx": __fd_glob_27, "platforms/veo.mdx": __fd_glob_28, "web-app/assets.mdx": __fd_glob_29, "web-app/characters.mdx": __fd_glob_30, "web-app/command-palette.mdx": __fd_glob_31, "web-app/compare.mdx": __fd_glob_32, "web-app/dashboard.mdx": __fd_glob_33, "web-app/index.mdx": __fd_glob_34, "web-app/projects.mdx": __fd_glob_35, "web-app/scenes.mdx": __fd_glob_36, "web-app/shots.mdx": __fd_glob_37, "web-app/timeline.mdx": __fd_glob_38, });
+export const docs = await create.doc("docs", "content/docs", {"index.mdx": __fd_glob_0, "api-reference/authentication.mdx": __fd_glob_1, "api-reference/extension-api.mdx": __fd_glob_2, "api-reference/index.mdx": __fd_glob_3, "api-reference/webhooks.mdx": __fd_glob_4, "chrome-extension/capture.mdx": __fd_glob_5, "chrome-extension/index.mdx": __fd_glob_6, "chrome-extension/installation.mdx": __fd_glob_7, "chrome-extension/platform-detection.mdx": __fd_glob_8, "chrome-extension/queue.mdx": __fd_glob_9, "chrome-extension/reuse.mdx": __fd_glob_10, "chrome-extension/side-panel.mdx": __fd_glob_11, "getting-started/extension-setup.mdx": __fd_glob_12, "getting-started/first-project.mdx": __fd_glob_13, "getting-started/index.mdx": __fd_glob_14, "getting-started/installation.mdx": __fd_glob_15, "concepts/asset-lifecycle.mdx": __fd_glob_16, "concepts/index.mdx": __fd_glob_17, "concepts/object-model.mdx": __fd_glob_18, "concepts/prompt-packages.mdx": __fd_glob_19, "concepts/rights-provenance.mdx": __fd_glob_20, "platforms/elevenlabs.mdx": __fd_glob_21, "platforms/freepik.mdx": __fd_glob_22, "platforms/index.mdx": __fd_glob_23, "platforms/midjourney.mdx": __fd_glob_24, "platforms/runway.mdx": __fd_glob_25, "platforms/sora.mdx": __fd_glob_26, "platforms/suno.mdx": __fd_glob_27, "platforms/veo.mdx": __fd_glob_28, "web-app/assets.mdx": __fd_glob_29, "web-app/characters.mdx": __fd_glob_30, "web-app/command-palette.mdx": __fd_glob_31, "web-app/compare.mdx": __fd_glob_32, "web-app/dashboard.mdx": __fd_glob_33, "web-app/index.mdx": __fd_glob_34, "web-app/projects.mdx": __fd_glob_35, "web-app/scenes.mdx": __fd_glob_36, "web-app/shots.mdx": __fd_glob_37, "web-app/timeline.mdx": __fd_glob_38, });
 
-export const meta = await create.meta("meta", "content/docs", {"meta.json": __fd_glob_39, "api-reference/meta.json": __fd_glob_40, "chrome-extension/meta.json": __fd_glob_41, "concepts/meta.json": __fd_glob_42, "getting-started/meta.json": __fd_glob_43, "platforms/meta.json": __fd_glob_44, "web-app/meta.json": __fd_glob_45, });
+export const meta = await create.meta("meta", "content/docs", {"meta.json": __fd_glob_39, "api-reference/meta.json": __fd_glob_40, "concepts/meta.json": __fd_glob_41, "chrome-extension/meta.json": __fd_glob_42, "getting-started/meta.json": __fd_glob_43, "platforms/meta.json": __fd_glob_44, "web-app/meta.json": __fd_glob_45, });
